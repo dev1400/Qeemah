5ee357d7c6fa0c5c24a8baac39e2e221b4bf0b39
mabdulwaheed <mabdulwaheed@kaarlp-318.kaartech.com>
2015-04-01 10:01:37 +0300

    Controller code

--- Qeemah/WebContent/sapui5tutorial1/Overview.controller.js
+++ Qeemah/WebContent/sapui5tutorial1/Overview.controller.js
@@ -5,35 +5,35 @@ sap.ui.controller("sapui5tutorial1.Overview", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf sapui5tutorial1.Overview
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
 * @memberOf sapui5tutorial1.Overview
 */
-//	onBeforeRendering: function() {
-//
-//	},
+	onBeforeRendering: function() {
+
+	},
 
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
 * @memberOf sapui5tutorial1.Overview
 */
-//	onAfterRendering: function() {
-//
-//	},
+	onAfterRendering: function() {
+
+	},
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 * @memberOf sapui5tutorial1.Overview
 */
-//	onExit: function() {
-//
-//	}
+	onExit: function() {
+
+	},
 	goToNextPage:function(){
 		var router = sap.ui.core.UIComponent.getRouterFor(this);
 		router.navTo("SecondPage", {id:1}, false);
