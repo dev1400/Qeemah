e38cee33d1b380c321732e21d6c2f956b04187b8
dev1400 <mabdulwaheed@kaarlp-318.kaartech.com>
2015-04-22 16:07:25 +0300

    Basic info page linked

--- Qeemah/WebContent/view/Overview.controller.js
+++ Qeemah/WebContent/view/Overview.controller.js
@@ -1,16 +1,19 @@
 jQuery.sap.require("com.sagia.common.ModelHelper");
 sap.ui.controller("com.sagia.view.Overview", {
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf com.sagia.view.Overview
-*/
-	onInit: function() {
-		
+	/**
+	 * Called when a controller is instantiated and its View controls (if
+	 * available) are already created. Can be used to modify the View before it
+	 * is displayed, to bind event handlers and do other one-time
+	 * initialization.
+	 * 
+	 * @memberOf com.sagia.view.Overview
+	 */
+	onInit : function() {
+
 		// Model Helper reference
 		this.oModelHelper = com.sagia.common.ModelHelper;
-		
+
 		this._oVboxSignIn = this.getView().byId("idVBoxSignIn");
 		this._oVboxUserInfo = this.getView().byId("idVBoxUserInformation");
 		this._oVboxQuestion1 = this.getView().byId("idVBoxQuestion1Content");
@@ -21,233 +24,270 @@ sap.ui.controller("com.sagia.view.Overview", {
 		this._oVboxQuestion6 = this.getView().byId("idVBoxQuestion6Content");
 		this._oVboxQuestion7 = this.getView().byId("idVBoxQuestion7Content");
 		this._oVboxQuestion8 = this.getView().byId("idVBoxQuestion8Content");
-		
-		this._oNewRegistrationMatrixLayout = this.getView().byId("idRegistrationMatrixLayout");
+
+		this._oNewRegistrationMatrixLayout = this.getView().byId(
+				"idRegistrationMatrixLayout");
 		this._oLoginVBoxLayout = this.getView().byId("idLoginVBoxLayout");
-		
-		this._oVboxInvestmentGuidelinesDialog = this.getView().byId("idInvestmentGuidelines");
-		this._oVboxInvestmentGuidelinesDialogFirstMsg = this.getView().byId("idInvestmentGuidelineMsg");
-		
-		this._oVboxQuestionsContent = this.getView().byId("idVBoxQuestionsContent");
+
+		this._oVboxInvestmentGuidelinesDialog = this.getView().byId(
+				"idInvestmentGuidelines");
+		this._oVboxInvestmentGuidelinesDialogFirstMsg = this.getView().byId(
+				"idInvestmentGuidelineMsg");
+
+		this._oVboxQuestionsContent = this.getView().byId(
+				"idVBoxQuestionsContent");
 		this._oVboxNISTLAF = this.getView().byId("idVBoxNISTLAF");
 		this._oRoadMapNISTLAF = this.getView().byId("NISTLAFSteps");
-		
-		
+
 		this._oCarousel = this.getView().byId("idImageCarousel");
-		
+
 		this._oVboxQuestions = this.getView().byId("idVBoxQuestionsContent");
 		this._oVBoxRegistration = this.getView().byId("idVBoxRegistration");
-		this._oHboxRegistrationSuccessMsg = this.getView().byId("idHBoxRegistrationSuccessMessage");
+		this._oHboxRegistrationSuccessMsg = this.getView().byId(
+				"idHBoxRegistrationSuccessMessage");
 		this._oVboxOrgReg = this.getView().byId("idVBoxOrgReg");
-		
+
 		this._oPage = this.getView().byId("idPage");
-		
-		this._userSignFragment = sap.ui.xmlfragment("idUserSignInFragment","com.sagia.view.fragments.signin", this.getView().getController());
 
+		this._userSignFragment = sap.ui.xmlfragment("idUserSignInFragment",
+				"com.sagia.view.fragments.signin", this.getView()
+						.getController());
 
-
-	},
-
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf com.sagia.view.Overview
-*/
-	onBeforeRendering: function() {
+		// Main page content VBOX
+		this._oidMainPageContent = this.getView().byId("idMainPageContent");
+		// Basic page content VBOX
+		this._oidBasicInfoContent = this.getView().byId("idBasicInfoContent");
 
 	},
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf com.sagia.view.Overview
-*/
-	onAfterRendering: function() {
+	/**
+	 * Similar to onAfterRendering, but this hook is invoked before the
+	 * controller's View is re-rendered (NOT before the first rendering!
+	 * onInit() is used for that one!).
+	 * 
+	 * @memberOf com.sagia.view.Overview
+	 */
+	onBeforeRendering : function() {
 
 	},
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf com.sagia.view.Overview
-*/
-	onExit: function() {
+	/**
+	 * Called when the View has been rendered (so its HTML is part of the
+	 * document). Post-rendering manipulations of the HTML could be done here.
+	 * This hook is the same one that SAPUI5 controls get after being rendered.
+	 * 
+	 * @memberOf com.sagia.view.Overview
+	 */
+	onAfterRendering : function() {
 
 	},
-	goToNextPage:function(){
+
+	/**
+	 * Called when the Controller is destroyed. Use this one to free resources
+	 * and finalize activities.
+	 * 
+	 * @memberOf com.sagia.view.Overview
+	 */
+	onExit : function() {
+
+	},
+	goToNextPage : function() {
 		var router = sap.ui.core.UIComponent.getRouterFor(this);
-		router.navTo("SecondPage", {id:1}, false);
+		router.navTo("SecondPage", {
+			id : 1
+		}, false);
 	},
-	handleOkButtonPress: function(){
-		
+	handleOkButtonPress : function() {
+
 		this._oVboxSignIn.setVisible(false);
 		this._oVboxQuestions.setVisible(true);
-		//this._oVboxUserInfo.setVisible(true);
+		// this._oVboxUserInfo.setVisible(true);
 	},
-	handleRegisterUserButtonPress: function(){
-		
-		var  oInputFirstName = this.getView().byId("idInputFirstName").getValue();
-    	var  oInputLastName = this.getView().byId("idInputLastName").getValue();
-    	var  oInputMobileNumber = this.getView().byId("idInputMobileNumber").getValue();
-    	var  oInputEmail = this.getView().byId("idInputEmail").getValue();
-    	var  oPassword = this.getView().byId("idInputPassword").getValue();
-    	var  oReEntryPassword = this.getView().byId("idInputRePassword").getValue();
-    	if(oInputFirstName.length > 0 && oInputLastName.length >0 && oInputMobileNumber.length > 0 && oInputEmail.length >0
-    			&& oPassword.length > 0 && oReEntryPassword.length >0){
-    		sap.m.MessageToast.show(this.oModelHelper.getText("RegistrationSuccessful"));
-    	}else{    		
-    		sap.m.MessageToast.show(this.oModelHelper.getText("PleaseEnterRequiredFields"));
-    	}
-		
-		/*this._oVBoxRegistration.setVisible(false);
-		this._oHboxRegistrationSuccessMsg.setVisible(true);
-		sap.m.MessageToast.show(this.oModelHelper.getText("RegistrationSuccessful"));
-		this.oModelHelper.registerUser();*/
-		/** Read deals in amendment collection and bind model to view
-	     */
-	   /* _bindDealsInAmendmentCollectionModel: function() {
-*/
-	        //var oDealsInAmendmentCollectionModel = com.sagia.common.ModelHelper.readDealsInAmendmentCollection();
+	handleRegisterUserButtonPress : function() {
 
-	       /* this._oTable.setModel(oDealsInAmendmentCollectionModel);
-	    },*/
-		
+		var oInputFirstName = this.getView().byId("idInputFirstName")
+				.getValue();
+		var oInputLastName = this.getView().byId("idInputLastName").getValue();
+		var oInputMobileNumber = this.getView().byId("idInputMobileNumber")
+				.getValue();
+		var oInputEmail = this.getView().byId("idInputEmail").getValue();
+		var oPassword = this.getView().byId("idInputPassword").getValue();
+		var oReEntryPassword = this.getView().byId("idInputRePassword")
+				.getValue();
+		if (oInputFirstName.length > 0 && oInputLastName.length > 0
+				&& oInputMobileNumber.length > 0 && oInputEmail.length > 0
+				&& oPassword.length > 0 && oReEntryPassword.length > 0) {
+			sap.m.MessageToast.show(this.oModelHelper
+					.getText("RegistrationSuccessful"));
+		} else {
+			sap.m.MessageToast.show(this.oModelHelper
+					.getText("PleaseEnterRequiredFields"));
+		}
+
+		/*
+		 * this._oVBoxRegistration.setVisible(false);
+		 * this._oHboxRegistrationSuccessMsg.setVisible(true);
+		 * sap.m.MessageToast.show(this.oModelHelper.getText("RegistrationSuccessful"));
+		 * this.oModelHelper.registerUser();
+		 */
+		/**
+		 * Read deals in amendment collection and bind model to view
+		 */
+		/*
+		 * _bindDealsInAmendmentCollectionModel: function() {
+		 */
+		// var oDealsInAmendmentCollectionModel =
+		// com.sagia.common.ModelHelper.readDealsInAmendmentCollection();
+		/*
+		 * this._oTable.setModel(oDealsInAmendmentCollectionModel); },
+		 */
+
 	},
-	handleCheckboxQuestion1Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion1Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion1.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion1.setVisible(false);
-		}		
+		}
 	},
-	handleCheckboxQuestion2Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion2Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion2.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion2.setVisible(false);
-		}	
+		}
 	},
-	handleCheckboxQuestion3Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion3Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion3.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion3.setVisible(false);
-		}	
+		}
 	},
-	handleCheckboxQuestion4Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion4Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion4.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion4.setVisible(false);
-		}	
+		}
 	},
-	handleCheckboxQuestion5Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion5Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion5.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion5.setVisible(false);
-		}	
+		}
 	},
-	handleCheckboxQuestion6Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion6Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion6.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion6.setVisible(false);
-		}	
+		}
 	},
-	handleCheckboxQuestion7Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion7Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion7.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion7.setVisible(false);
-		}	
+		}
 	},
-	handleCheckboxQuestion8Change: function(oEvent){
-		if(oEvent.getParameter("selected")){
+	handleCheckboxQuestion8Change : function(oEvent) {
+		if (oEvent.getParameter("selected")) {
 			this._oVboxQuestion8.setVisible(true);
-		}else{
+		} else {
 			this._oVboxQuestion8.setVisible(false);
-		}	
+		}
 	},
-	handleIconTabBarSelect: function(oEvent){
+	handleIconTabBarSelect : function(oEvent) {
 		var oPricingTabBar = this.getView().byId("idIconTabBarBase");
 		var sSelectedTab = oPricingTabBar.getSelectedKey();
-		
+
 		console.log(sSelectedTab);
 	},
-	handleProcessButtonPress: function(oEvent){
+	handleProcessButtonPress : function(oEvent) {
 		this._oVboxQuestionsContent.setVisible(false);
 		this._oVboxNISTLAF.setVisible(true);
 	},
-	handleRoadMapSection: function(oEvent){
+	handleRoadMapSection : function(oEvent) {
 		console.log(oEvent.getParameters.stepId);
 	},
-	handleIconTabBarItemSelect: function(oEvent){
-		if(oEvent.getParameters().key == '1'){
+	handleIconTabBarItemSelect : function(oEvent) {
+		if (oEvent.getParameters().key == '1') {
 			this._oCarousel.setVisible(true);
-		}else{
-			this._oCarousel.setVisible(false);			
+		} else {
+			this._oCarousel.setVisible(false);
 		}
 	},
-	handleProceedToOrgRegButtonPress: function(oEvent) {
+	handleProceedToOrgRegButtonPress : function(oEvent) {
 
-        var oMoreLink = oEvent.getSource();
+		var oMoreLink = oEvent.getSource();
 
-        // create pop over fragment only once
-        if (!this._popOverFragment) {
-            this._popOverFragment = sap.ui.xmlfragment(
-                "com.sagia.view.fragments.registration", this.getView().getController());
-            this.getView().addDependent(this._popOverFragment);
-        }
+		// create pop over fragment only once
+		if (!this._popOverFragment) {
+			this._popOverFragment = sap.ui.xmlfragment(
+					"com.sagia.view.fragments.registration", this.getView()
+							.getController());
+			this.getView().addDependent(this._popOverFragment);
+		}
 
-        // var fragmentTextView = sap.ui.getCore().byId("idFragmentTextView");
-        //fragmentTextView.setText(oEvent.getSource().getBindingContext().getObject().Description);
+		// var fragmentTextView = sap.ui.getCore().byId("idFragmentTextView");
+		// fragmentTextView.setText(oEvent.getSource().getBindingContext().getObject().Description);
 
-        this._popOverFragment.open();
-    },
-    handleProceedtoOrganizationRegistrationPress: function(){
-    	this._popOverFragment.close();
-    	this._oHboxRegistrationSuccessMsg.setVisible(false);
-    	this._oVboxOrgReg.setVisible(true);
-    },
-    handleReadInvestmentGuidelinePress: function(){
-    	//this._oVboxInvestmentGuidelinesDialog.setVisible(true);
-    	//this._oVboxInvestmentGuidelinesDialogFirstMsg.setVisible(false);
-    	sap.ui.getCore().byId("idInvestmentGuidelines").setVisible(true);
-    	sap.ui.getCore().byId("idInvestmentGuidelineMsg").setVisible(false);
-    },
-    handleSignInButtonPress: function(oEvent){
-    	
-    	var userID = this.getView().byId("idSignInUsernameInput").getValue();
-    	var password = this.getView().byId("idSignInPasswordInput").getValue();
-    	if(userID.length > 0 && password.length >0){
-    		sap.m.MessageToast.show(this.oModelHelper.getText("SignInSuccessful"));
-    	}else{    		
-    		sap.m.MessageToast.show(this.oModelHelper.getText("PleaseEnterRequiredFields"));
-    	}
-    	//this.oModelHelper.signInUser(userID,password);
-    	/*var credentialsStatus=this.oModelHelper.signInUser(userID,password);
-    	console.log(credentialsStatus);*/
-    	/*if(credentialsStatus){
-    		sap.m.MessageToast.show(this.oModelHelper.getText("SignInSuccessful"));
-    	}els{    		
-    		sap.m.MessageToast.show(this.oModelHelper.getText("InvalidCredentials"));
-    	}*/
-    },
-    handleCancelButtonPress: function(oEvent){
-    },
-    
-    /**
-     * home page registration button press
-     */
-    handleNewRegistrationButtonPress: function(){
-    	this._oLoginVBoxLayout.setVisible(false);
-    	this._oNewRegistrationMatrixLayout.setVisible(true);
-    },
-    /**
-     * home page registration button press
-     */
-    handleLoginButtonPress: function(){    	
-    	this._oNewRegistrationMatrixLayout.setVisible(false);
-    	this._oLoginVBoxLayout.setVisible(true);
-    }
+		this._popOverFragment.open();
+	},
+	handleProceedtoOrganizationRegistrationPress : function() {
+		this._popOverFragment.close();
+		this._oHboxRegistrationSuccessMsg.setVisible(false);
+		this._oVboxOrgReg.setVisible(true);
+	},
+	handleReadInvestmentGuidelinePress : function() {
+		// this._oVboxInvestmentGuidelinesDialog.setVisible(true);
+		// this._oVboxInvestmentGuidelinesDialogFirstMsg.setVisible(false);
+		sap.ui.getCore().byId("idInvestmentGuidelines").setVisible(true);
+		sap.ui.getCore().byId("idInvestmentGuidelineMsg").setVisible(false);
+	},
+	handleSignInButtonPress : function(oEvent) {
+
+		var userID = this.getView().byId("idSignInUsernameInput").getValue();
+		var password = this.getView().byId("idSignInPasswordInput").getValue();
+		if (userID.length > 0 && password.length > 0) {
+			sap.m.MessageToast.show(this.oModelHelper
+					.getText("SignInSuccessful"));
+			
+			this._oidMainPageContent.setVisible(false);
+			this._oidBasicInfoContent.setVisible(true);
+		} else {
+			sap.m.MessageToast.show(this.oModelHelper
+					.getText("PleaseEnterRequiredFields"));
+		}
+		// this.oModelHelper.signInUser(userID,password);
+		/*
+		 * var credentialsStatus=this.oModelHelper.signInUser(userID,password);
+		 * console.log(credentialsStatus);
+		 */
+		/*
+		 * if(credentialsStatus){
+		 * sap.m.MessageToast.show(this.oModelHelper.getText("SignInSuccessful"));
+		 * }els{
+		 * sap.m.MessageToast.show(this.oModelHelper.getText("InvalidCredentials")); }
+		 */
+	},
+	handleCancelButtonPress : function(oEvent) {
+	},
+
+	/**
+	 * home page registration button press
+	 */
+	handleNewRegistrationButtonPress : function() {
+		this._oLoginVBoxLayout.setVisible(false);
+		this._oNewRegistrationMatrixLayout.setVisible(true);
+	},
+	/**
+	 * home page registration button press
+	 */
+	handleLoginButtonPress : function() {
+		this._oNewRegistrationMatrixLayout.setVisible(false);
+		this._oLoginVBoxLayout.setVisible(true);
+	}
 });
